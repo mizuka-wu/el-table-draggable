c923fc4088b228172ac2eca6b9d9edd4443cc506
水华 <mizuka.wu@outlook.com>
2021-07-14 23:33:22 +0800

    feat: 打包

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ElTableDraggable></ElTableDraggable>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ElTableDraggable from './components/ElTableDraggable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ElTableDraggable
   }
 }
 </script>
 
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  margin: 0;
 }
 </style>
