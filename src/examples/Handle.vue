e883b75f78a1f0cca71360550fd8d2732dbada7a
水华 <mizuka.wu@outlook.com>
2021-07-15 21:41:02 +0800

    feat: 拖拽

---
+++ src/examples/Handle.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <ElTableDraggable handle=".el-icon-rank">
+      <el-table row-key="id" :data="list">
+        <el-table-column width="30">
+          <i class="el-icon-rank" />
+        </el-table-column>
+        <el-table-column
+          :label="column.key"
+          :key="column.key"
+          :prop="column.key"
+          v-for="column of columns"
+        ></el-table-column>
+      </el-table>
+    </ElTableDraggable>
+    <ListViewer :value="list" />
+    <CodeViewer language="html" :code="code"></CodeViewer>
+  </div>
+</template>
+
+<script>
+export const name = "Handle可拖拽"
+import { createData, columns } from '../utils/createTable'
+export default {
+    data() {
+        return {
+            columns,
+            list: createData(),
+            code: `<ElTableDraggable handle=".el-icon-rank">
+    <el-table row-key="必填" :data="list">
+      <el-table-column>
+        <i class="el-icon-rank" />
+      </el-table-column>
+    </el-table>
+</ElTableDraggable>`
+        }
+    }
+}
+</script>
