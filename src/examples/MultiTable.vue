519f387c06da96efc86acdf09ae78113575714d1
水华 <mizuka.wu@outlook.com>
2021-07-15 21:12:22 +0800

    feat: 多表格互相拖拽

---
+++ src/examples/MultiTable.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :key="index" :span="24/lists.length" v-for="(list, index) of lists">
+      <ElTableDraggable group="multiTable">
+        <el-table row-key="id" :data="list">
+          <el-table-column
+            :label="column.key"
+            :key="column.key"
+            :prop="column.key"
+            v-for="column of columns"
+          ></el-table-column>
+        </el-table>
+      </ElTableDraggable>
+      <ListViewer :value="list" />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export const name = "多表格相互拖拽"
+import { createData, columns } from '../utils/createTable'
+export default {
+    data() {
+        return {
+            columns,
+            lists: Array.from(new Array(2)).map((key, index) => createData(index + 2))
+        }
+    }
+}
+</script>
