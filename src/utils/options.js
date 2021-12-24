d916a2f3a2607d92254cdb5414a8a8bc60aef970
mizuka.wu <mizuka.wu@outlook.com>
2021-12-24 15:35:44 +0800

    feat: 解决关闭，拖拽后因为自动default-expanded-all导致的问题

--- src/utils/options.js
+++ src/utils/options.js
@@ -228,15 +228,16 @@ export const CONFIG = {
       return {
         onStart(evt) {
           /**
-           * 全局暂停监听dom
-           *
-           * @todo 解决手动关闭后会有的错位问题
-           * 导致原因，default-expanded-all
-           * 需要记录一下当前打开的行，结束之后还原状态
+           * 1. 全局暂停监听dom
+           * 2. @todo 解决手动关闭后会有的错位问题
+           *    导致原因，default-expanded-all
+           *    需要记录一下当前打开的行，结束之后还原状态（待定）
            */
           for (const draggableTable of context.values()) {
             draggableTable[DOM_MAPPING_NAME] &&
               draggableTable[DOM_MAPPING_NAME].stop();
+
+            draggableTable.store.states.defaultExpandAll = false
           }
 
           /**
