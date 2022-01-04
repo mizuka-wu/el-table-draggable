5d8bc990be302d19abba07b554abce2f68d858dc
Edward Mizuka <mizuka.wu@outlook.com>
2022-01-04 11:10:10 +0800

    Update DataTreeSampleParent.vue

--- src/examples/DataTreeSampleParent.vue
+++ src/examples/DataTreeSampleParent.vue
@@ -16,47 +16,45 @@
 </template>
 
 <script>
-  export const name = "树状表格拖拽, 仅支持同个父级";
-  export const nameEn = "Tree Table Draggable, only with same parent";
-  import { createData, columns } from "../utils/createTable";
+  import { createData, columns } from '../utils/createTable';
+
+  export const name = '树状表格拖拽, 仅支持同个父级';
+  export const nameEn = 'Tree Table Draggable, only with same parent';
+
   export default {
     data() {
       return {
         columns,
-        list: createData(2).map((item) => ({
+        list: createData(2).map(item => ({
           index: item.index + 1,
-          children: createData(2).map((data) => ({
+          children: createData(2).map(data => ({
             index: `${item.index + 1}-${data.index + 1}`,
-            children: createData(4).map((subData) => ({
-              index: `${item.index + 1}-${data.index + 1}-${subData.index + 1}`,
-            })),
-          })),
+            children: createData(4).map(subData => ({
+              index: `${item.index + 1}-${data.index + 1}-${subData.index + 1}`
+            }))
+          }))
         })),
         code: `
         <template>
-        <ElTableDraggable>
-    <el-table row-key="必填" :data="list" :onMove="oMove">
-    </el-table>
-</ElTableDraggable>
-</template>
-<script>
-export default {
-  methods: {
-        onMove(evt, originEvt, { dragged, related }) {
-        if (dragged.level !== 2) {
-          return false;
-        }
+            <ElTableDraggable v-on="$listeners" :onMove="onMove">
+              <el-table :data="list" row-key="index" default-expand-all></el-table>
+            </ElTableDraggable>
+        </template>
+        
+            methods: {
+              onMove(evt, originEvt, { dragged, related }) {
+                if (dragged.level !== 2) {
+                  return false;
+                }
 
-        if (dragged.parent === related.parent) {
-          return true;
-        }
+                if (dragged.parent === related.parent) {
+                  return true;
+                }
 
-        return false;
-      },
-  }
-}
-</script>
-`,
+                return false;
+              }
+            }
+        `
       };
     },
     methods: {
@@ -70,7 +68,7 @@ export default {
         }
 
         return false;
-      },
-    },
+      }
+    }
   };
 </script>
